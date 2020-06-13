38c9e6dec5e9b401b7d81adaec7a607932dfd06b
Henrik Steen <henrist@henrist.net>
2020-06-13 23:14:54 +0200

    Add tags with aspect

--- bin/aspect-tag-issue.ts
+++ bin/aspect-tag-issue.ts
@@ -1,6 +1,19 @@
 #!/usr/bin/env node
-import * as cdk from '@aws-cdk/core';
-import { AspectTagIssueStack } from '../lib/aspect-tag-issue-stack';
+import * as cdk from "@aws-cdk/core"
+import { AspectTagIssueStack } from "../lib/aspect-tag-issue-stack"
 
-const app = new cdk.App();
-new AspectTagIssueStack(app, 'AspectTagIssueStack');
+const app = new cdk.App()
+new AspectTagIssueStack(app, "AspectTagIssueStack")
+
+app.node.applyAspect({
+  visit(construct: cdk.IConstruct) {
+    if (cdk.Construct.isConstruct(construct)) {
+      const stack = construct.node.scopes.find((it): it is cdk.Stack =>
+        cdk.Stack.isStack(it),
+      )
+      if (stack != null) {
+        cdk.Tag.add(construct, "Test", "Value")
+      }
+    }
+  },
+})
